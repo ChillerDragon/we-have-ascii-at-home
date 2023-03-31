93acb5a50b20dde73443838ae32966f66f228ae8
ChillerDragon <ChillerDragon@gmail.com>
2023-03-31 17:36:25 +0200

    Rust makes you write nice code they said ...

--- whaah-backend/src/main.rs
+++ whaah-backend/src/main.rs
@@ -1,5 +1,9 @@
 use actix_web::{get, web, App, HttpServer, Responder};
+use chrono::{Local, DateTime};
+use actix_web::HttpRequest;
 use std::fs;
+use sqlite;
+use sqlite::State;
 
 #[get("/")]
 async fn index() -> impl Responder {
@@ -11,7 +15,7 @@ async fn index() -> impl Responder {
 async fn casts() -> impl Responder {
     println!("GET /casts");
     let mut paths: Vec<String> = Vec::new();
-    for entry in fs::read_dir("../casts").unwrap() {
+    for entry in fs::read_dir("../frontend/casts").unwrap() {
         let entry = entry.unwrap();
         let path = entry.path();
         if path.is_dir() {
@@ -24,9 +28,88 @@ async fn casts() -> impl Responder {
     return json::stringify(paths);
 }
 
-#[get("/a/{name}")]
-async fn hello(name: web::Path<String>) -> impl Responder {
-    format!("Hello {}!", &name)
+#[get("/get_views/{name}")]
+async fn get_views(cast: web::Path<String>, req: HttpRequest) -> impl Responder {
+    let connection = sqlite::open("../db/whaah.db").unwrap();
+    let query = "SELECT ID FROM casts WHERE Filename = ?";
+
+    let mut stmt = connection.prepare(query).unwrap();
+    stmt.bind((1, cast.to_string().as_str())).unwrap();
+
+    while let Ok(State::Row) = stmt.next() {
+        let cast_id: i64 = stmt.read::<i64, _>("ID").unwrap();
+        let user_agent = req.headers().get("user-agent").unwrap().to_str().unwrap();
+        let ip_addr = req.peer_addr().unwrap().to_string();
+        let now: DateTime<Local> = Local::now();
+        let ts: String = now.to_string();
+        println!("Requesting view for cast_id={} ip={} user_agent={} cast={}", cast_id, &ip_addr, &user_agent, &cast);
+        // TODO: bind this properly before mr bobby tables sees this
+        //       also this whole code is such a hack
+        let query = format!("select count(*) as views from views where CastID = {};", cast_id);
+        let mut views = 0;
+        connection
+            .iterate(query, |pairs| {
+                for &(name, value) in pairs.iter() {
+                    views = value.unwrap().parse().unwrap();
+                    return true;
+                }
+                true
+            })
+            .unwrap();
+        return format!("{{\"views\": {}}}", &views);
+
+        // let query = "select count(*) as views from views where CastID = ?;";
+        // let mut stmt = connection.prepare(query).unwrap();
+        // while let Ok(State::Row) = stmt.next() {
+        //     let views: i64 = stmt.read::<i64, _>("views").unwrap();
+        //     return format!("{{\"views\": {}}}", &views);
+        // }
+    }
+    return format!("{{\"error\": \"failed to get views for {}\"}}", &cast);
+}
+
+// Horrible api endpoints i know...
+// This should be a post request or something like that
+// But I want to later add some dirty hacks to track views
+// Even from browsers without javascript
+// by loading this url with a link or image tag
+#[get("/view/{name}")]
+async fn view(cast: web::Path<String>, req: HttpRequest) -> impl Responder {
+    let connection = sqlite::open("../db/whaah.db").unwrap();
+    let query = "SELECT ID FROM casts WHERE Filename = ?";
+
+    let mut stmt = connection.prepare(query).unwrap();
+    stmt.bind((1, cast.to_string().as_str())).unwrap();
+
+    while let Ok(State::Row) = stmt.next() {
+        let cast_id: i64 = stmt.read::<i64, _>("ID").unwrap();
+        let user_agent = req.headers().get("user-agent").unwrap().to_str().unwrap();
+        let ip_addr = req.peer_addr().unwrap().to_string();
+        let now: DateTime<Local> = Local::now();
+        let ts: String = now.to_string();
+        println!("Adding view for cast_id={} ip={} user_agent={} cast={}", cast_id, &ip_addr, &user_agent, &cast);
+        let query = concat!(
+            "INSERT INTO views ",
+            "(CastID, IP, Timestamp, UserAgent, Tracker, Ref) ",
+            "VALUES ",
+            "(?     , ? , ?        , ?        , ?      , ?)");
+        let mut stmt = connection.prepare(query).unwrap();
+        stmt.bind((1, cast_id));
+        stmt.bind((2, ip_addr.as_str()));
+        stmt.bind((3, ts.as_str()));
+        stmt.bind((4, user_agent));
+        stmt.bind((5, "todo tracker"));
+        stmt.bind((6, "todo ref"));
+        while let Ok(State::Row) = stmt.next() {
+            println!("Failed to insert view\n");
+            return format!("{{\"error\": \"failed to add view for {}\"}}", &cast);
+        }
+        println!("Inserted view\n");
+        return format!("{{\"message\": \"add view for {}\"}}", &cast);
+    }
+
+    println!("Tried to view invalid cast: {}", &cast);
+    return format!("{{\"error\": \"failed to add view for {}\"}}", &cast);
 }
 
 #[actix_web::main]
@@ -34,7 +117,8 @@ async fn main() -> std::io::Result<()> {
     println!("Starting backend at http://127.0.0.1:8080 ...\n");
     HttpServer::new(|| App::new()
                     .service(index)
-                    .service(hello)
+                    .service(view)
+                    .service(get_views)
                     .service(casts))
         .bind(("127.0.0.1", 8080))?
         .run()
